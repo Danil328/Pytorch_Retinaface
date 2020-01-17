9d7ce4e794237b70cd3845b7368d45112272d87a
danil328 <danil328@yandex.ru>
2020-01-18 00:16:44 +0300

    add setup.py

--- retinaface/detect.py
+++ retinaface/detect.py
@@ -1,16 +1,18 @@
 from __future__ import print_function
-import os
+
 import argparse
+import time
+
+import cv2
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-import numpy as np
+
 from data import cfg_mnet, cfg_re50
 from layers.functions.prior_box import PriorBox
-from utils.nms.py_cpu_nms import py_cpu_nms
-import cv2
-from models.retinaface import RetinaFace
+from models.retinaface import RetinaFace, load_weights
 from utils.box_utils import decode, decode_landm
-import time
+from utils.nms.py_cpu_nms import py_cpu_nms
 
 parser = argparse.ArgumentParser(description='Retinaface')
 
@@ -26,43 +28,6 @@ parser.add_argument('-s', '--save_image', action="store_true", default=True, hel
 parser.add_argument('--vis_thres', default=0.6, type=float, help='visualization_threshold')
 args = parser.parse_args()
 
-
-def check_keys(model, pretrained_state_dict):
-    ckpt_keys = set(pretrained_state_dict.keys())
-    model_keys = set(model.state_dict().keys())
-    used_pretrained_keys = model_keys & ckpt_keys
-    unused_pretrained_keys = ckpt_keys - model_keys
-    missing_keys = model_keys - ckpt_keys
-    print('Missing keys:{}'.format(len(missing_keys)))
-    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
-    print('Used keys:{}'.format(len(used_pretrained_keys)))
-    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
-    return True
-
-
-def remove_prefix(state_dict, prefix):
-    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
-    print('remove prefix \'{}\''.format(prefix))
-    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
-    return {f(key): value for key, value in state_dict.items()}
-
-
-def load_model(model, pretrained_path, load_to_cpu):
-    print('Loading pretrained model from {}'.format(pretrained_path))
-    if load_to_cpu:
-        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
-    else:
-        device = torch.cuda.current_device()
-        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
-    if "state_dict" in pretrained_dict.keys():
-        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
-    else:
-        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
-    check_keys(model, pretrained_dict)
-    model.load_state_dict(pretrained_dict, strict=False)
-    return model
-
-
 if __name__ == '__main__':
     torch.set_grad_enabled(False)
     cfg = None
@@ -71,8 +36,8 @@ if __name__ == '__main__':
     elif args.network == "resnet50":
         cfg = cfg_re50
     # net and model
-    net = RetinaFace(cfg=cfg, phase = 'test')
-    net = load_model(net, args.trained_model, args.cpu)
+    net = RetinaFace(cfg=cfg, phase='test')
+    net = load_weights(net, args.trained_model, args.cpu)
     net.eval()
     print('Finished loading model!')
     print(net)
@@ -165,4 +130,3 @@ if __name__ == '__main__':
 
             name = "test.jpg"
             cv2.imwrite(name, img_raw)
-
---
+++ retinaface/inference.py
@@ -0,0 +1,102 @@
+from typing import List
+
+import gdown
+import numpy as np
+import torch
+
+from data import cfg_re50
+from layers.functions.prior_box import PriorBox
+from models.retinaface import RetinaFace, load_weights
+from utils.box_utils import decode, decode_landm
+from utils.nms.py_cpu_nms import py_cpu_nms
+
+
+class FaceDetector(object):
+    def __init__(self, weights: str = "https://drive.google.com/uc?id=14KX6VqF69MdSPk3Tr9PlDYbq7ArpdNUW",
+                 confidence: float = 0.02, top_k: int = 5000, nms_threshold: float = 0.4, keep_top_k: int = 750,
+                 cpu: bool = False, padding: float = 0.2):
+        self.confidence = confidence
+        self.top_k = top_k
+        self.nms_threshold = nms_threshold
+        self.keep_top_k = keep_top_k
+        self.padding = padding
+
+        if 'http' in weights:
+            weights = self.download_weights(weights)
+        self.model = RetinaFace(cfg=cfg_re50, phase="test")
+        self.model = load_weights(self.model, weights, load_to_cpu=cpu)
+        self.model.eval()
+
+        self.device = torch.device("cpu" if cpu else "cuda")
+        self.model.to(self.device)
+
+    @staticmethod
+    def download_weights(url):
+        gdown.download(url, 'weights.pth', quiet=False)
+        return 'weights.pth'
+
+    def get_faces(self, image: np.ndarray) -> (List[np.ndarray], List[float]):
+        img = image
+        image = image.astype(np.float32)
+        im_height, im_width, _ = image.shape
+        scale = torch.Tensor([image.shape[1], image.shape[0], image.shape[1], image.shape[0]])
+        image -= (104, 117, 123)
+        image = image.transpose(2, 0, 1)
+        image = torch.from_numpy(image).unsqueeze(0)
+        image = image.to(self.device)
+        scale = scale.to(self.device)
+
+        with torch.no_grad():
+            loc, conf, landms = self.model(image)  # forward pass
+
+        priorbox = PriorBox(cfg_re50, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(self.device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, cfg_re50['variance'])
+        boxes = boxes * scale
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, cfg_re50['variance'])
+        scale1 = torch.Tensor([image.shape[3], image.shape[2], image.shape[3], image.shape[2],
+                               image.shape[3], image.shape[2], image.shape[3], image.shape[2],
+                               image.shape[3], image.shape[2]])
+        scale1 = scale1.to(self.device)
+        landms = landms * scale1
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > self.confidence)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:self.top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, self.nms_threshold)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:self.keep_top_k, :]
+        landms = landms[:self.keep_top_k, :]
+
+        dets = np.concatenate((dets, landms), axis=1)
+
+        faces = []
+        scores = []
+        for b in dets:
+            scores.append(b[4])
+            b = list(map(int, b))
+            padding_height = int((b[2] - b[0]) * self.padding)
+            padding_width = int((b[3] - b[1]) * self.padding)
+
+            faces.append(img[max(int(b[1] - padding_height), 0):int(b[3]) + padding_height,
+                                max(int(b[0]) - padding_width, 0):int(b[2]) + padding_width, :])
+        return faces, scores
