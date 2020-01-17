feb538b72c11b4ed4edf8677c50495eae91f21ae
danil328 <danil328@yandex.ru>
2020-01-18 00:33:21 +0300

    add setup.py

--- retinaface/__init__.py
+++ retinaface/__init__.py
@@ -1,6 +1,9 @@
 __version__ = '0.0.1'
 
 from .face_detector import FaceDetector
+from . import layers
+from . import models
+from . import utils
+from . import data
 
-__all__ = [
-    'FaceDetector']
+__all__ = ['FaceDetector', 'layers', 'models', 'utils', 'data']
